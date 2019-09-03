72362cbf69b43b99e25b682b6bf41c16498186cd
Slawomir Wojcik <wojciks@gmail.com>
2019-09-03 16:40:14 +0200

    added reading from excel file, config file, txt creation basing on templates

---
+++ excel_process.py
@@ -0,0 +1,56 @@
+import xlrd
+from appconf import DATA_GOING_DOWN, FIRST_DATA, EXCEL_PATH, EXCEL_SHEET, EVENT_LOCATION_DICT
+
+
+def excel_data_source(data_going_down, where_first_data):
+    if data_going_down:
+        pass
+    file = EXCEL_PATH
+    workbook = xlrd.open_workbook(file)
+    worksheet = workbook.sheet_by_name(EXCEL_SHEET)
+    excel_data = {
+        '~HFOROB~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['HFOROB']), worksheet)),
+        '~MDOROB~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MDOROB']), worksheet)),
+        '~LOCYLROB~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['LOCYLROB']), worksheet)),
+        '~LOMEROB~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['LOMEROB']), worksheet)),
+        '~LOAUXROB~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['LOAUXROB']), worksheet)),
+        '~LOTOTALROB~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['LOTOTALROB']), worksheet)),
+        '~FWROB~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['FWROB']), worksheet)),
+        # '~FWPROD~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['FWPROD']), worksheet)),
+        # '~FWCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['FWCONS']), worksheet)),
+        '~MEHFOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MEHFOCONS']), worksheet)),
+        '~MEMDOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MEMDOCONS']), worksheet)),
+        '~AUXHFOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['AUXHFOCONS']), worksheet)),
+        '~AUXMDOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['AUXMDOCONS']), worksheet)),
+        '~BOILERHFOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['BOILERHFOCONS']), worksheet)),
+        '~BOILERMDOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['BOILERMDOCONS']), worksheet)),
+        '~TOTALHFOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['TOTALHFOCONS']), worksheet)),
+        '~TOTALMDOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['TOTALMDOCONS']), worksheet)),
+        '~LOCYLCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['LOCYLCONS']), worksheet)),
+        '~LOMECONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['LOMECONS']), worksheet)),
+        '~LOAUXCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['LOAUXCONS']), worksheet)),
+        # '~TOTALLOCONS~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['TOTALLOCONS']), worksheet)),
+        '~RPM~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['RPM']), worksheet)),
+        '~MEDIST~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MEDIST']), worksheet)),
+        '~MESPD~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MESPD']), worksheet)),
+        '~SLIP~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['SLIP']), worksheet)),
+        '~MEKW~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MEKW']), worksheet)),
+        '~MEKWH~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MEKWH']), worksheet)),
+        '~MELOAD~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MELOAD']), worksheet)),
+        '~MEGOV~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['MEGOV']), worksheet)),
+        '~AUXTIME~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['AUXTIME']), worksheet)),
+        '~AUXKW~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['AUXKW']), worksheet)),
+        '~AUXKWH~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['AUXKWH']), worksheet)),
+        '~SLUDGETK~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['SLUDGETK']), worksheet)),
+        '~OILYBILGETK~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['OILYBILGETK']), worksheet)),
+        '~INCINERATORSETTLINGTK~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['INCINERATORSETTLINGTK']), worksheet)),
+        '~INCINERATORSERVICETK~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['INCINERATORSERVICETK']), worksheet)),
+        '~BILGEWATERTK~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['BILGEWATERTK']), worksheet)),
+        '~SLUDGETOTAL~': str(single_cell_data_read((where_first_data, EVENT_LOCATION_DICT['SLUDGETOTAL']), worksheet)),
+    }
+    return excel_data
+
+
+def single_cell_data_read(data_loc, worksheet):
+    return worksheet.cell(data_loc[0], data_loc[1]).value
+
---
+++ text_file_process.py
@@ -0,0 +1,46 @@
+import datetime
+from excel_process import excel_data_source
+from appconf import DATA_GOING_DOWN, FIRST_DATA, EXCEL_PATH, EXCEL_SHEET, EVENT_LOCATION_DICT
+
+
+voy = 325
+event = "NOON"
+location = "At sea"
+captain = "ION BUCUR"
+to_whom = 'Charterer'
+
+DICTIONARY = {
+    '~VOY~': voy,
+    '~EVENT~': event,
+    '~LOCATION~': location,
+    '~MASTER~': captain
+}
+
+
+def merge_two_dicts(x, y):
+    z = x.copy()   # start with x's keys and values
+    z.update(y)    # modifies z with y's keys and values & returns None
+    return z
+
+
+def txt_file_creation(to_whom, which_event, message):
+    timestamp = datetime.datetime.now()  # only temporary, need to take date od the actual event.
+    naming = to_whom + " " + which_event + " report "
+    filename = timestamp.strftime(naming + "%Y-%m-%d.txt")
+    with open(filename, "w") as myfileresult:
+        myfileresult.write(message)
+    myfileresult.close()
+
+
+def template_text_file_read(file_path):
+    with open(file_path, 'r') as file_buffer:
+        filedata = file_buffer.read()
+    for key in DICTIONARY:
+        filedata = filedata.replace(key, str(DICTIONARY[key]))
+    return filedata
+
+
+DICTIONARY = merge_two_dicts(DICTIONARY, excel_data_source(DATA_GOING_DOWN, FIRST_DATA))
+
+txt_file_creation(to_whom, event,
+                  template_text_file_read('./reps_example/CHARTERER_EXAMPLE.txt'))
